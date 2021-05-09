37853a85701010aa8f9e4e2d25d1e42563831194
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-09 23:32:55 +0600

    userMiddleware and adminMiddleware created & getCategories updated to return nested categories

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const { requiredSignin, adminMiddleware } = require('../common-middleware');
 const { addCategory, getCategories } = require('../controllers/category');
 const router = express.Router();
 
-router.post('/category/create', addCategory);
+router.post('/category/create', requiredSignin, adminMiddleware, addCategory);
 
 router.get('/category/getcategory', getCategories);
 
