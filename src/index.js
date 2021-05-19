a29b29d0d064dfee2f19855f644f6cad102dc56e
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-19 15:26:23 +0600

    initalData route added

--- src/index.js
+++ src/index.js
@@ -1,40 +1,45 @@
-const express = require('express');
-const env = require('dotenv');
+const express = require("express");
+const env = require("dotenv");
 const app = express();
-const mongoose = require('mongoose');
-const path = require('path');
-const cors = require('cors');
+const mongoose = require("mongoose");
+const path = require("path");
+const cors = require("cors");
 
 // routes
-const authRoutes = require('./routes/auth');
-const adminRoutes = require('./routes/admin/auth');
-const categoryRoutes = require('./routes/category');
-const productRoutes = require('./routes/product');
-const cartRoutes = require('./routes/cart');
+const authRoutes = require("./routes/auth");
+const adminRoutes = require("./routes/admin/auth");
+const initialDataRoutes = require("./routes/admin/initialData");
+const categoryRoutes = require("./routes/category");
+const productRoutes = require("./routes/product");
+const cartRoutes = require("./routes/cart");
 
 // environment variables
 env.config();
 
 // mongodb connection
-mongoose.connect(
-    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.4l57g.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`, 
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.4l57g.mongodb.net/${process.env.MONGO_DB_DATABASE}?retryWrites=true&w=majority`,
     {
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
-    useCreateIndex: true
-    }).then(() => {
-        console.log('database connected')
-    })
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    }
+  )
+  .then(() => {
+    console.log("database connected");
+  });
 
 app.use(cors());
 app.use(express.json());
-app.use('/public', express.static(path.join(__dirname, 'uploads')));
-app.use('/api', authRoutes);
-app.use('/api', adminRoutes);
-app.use('/api', categoryRoutes);
-app.use('/api', productRoutes);
-app.use('/api', cartRoutes);
+app.use("/public", express.static(path.join(__dirname, "uploads")));
+app.use("/api", authRoutes);
+app.use("/api", adminRoutes);
+app.use("/api", categoryRoutes);
+app.use("/api", productRoutes);
+app.use("/api", cartRoutes);
+app.use("/api", initialDataRoutes);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server is running on port ${process.env.PORT}`);
-})
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
