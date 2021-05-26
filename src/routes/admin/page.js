0e9ad9ca1a4edb80832580f40f6cb922be51ab23
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-26 14:50:01 +0600

    admin page controller - createPage updated and getPage added

--- src/routes/admin/page.js
+++ src/routes/admin/page.js
@@ -1,11 +1,20 @@
-const express = require('express');
-const { upload, requiredSignin, adminMiddleware } = require('../../common-middleware');
-const { createPage } = require('../../controllers/admin/page');
+const express = require("express");
+const {
+  upload,
+  requiredSignin,
+  adminMiddleware,
+} = require("../../common-middleware");
+const { createPage, getPage } = require("../../controllers/admin/page");
 const router = express.Router();
 
-router.post('/page/create', requiredSignin, adminMiddleware, upload.fields([
-    { name: 'banners'},
-    { name: 'products' }
-]), createPage);
+router.post(
+  "/page/create",
+  requiredSignin,
+  adminMiddleware,
+  upload.fields([{ name: "banners" }, { name: "products" }]),
+  createPage
+);
+
+router.get(`/page/:category/:type`, getPage);
 
 module.exports = router;
