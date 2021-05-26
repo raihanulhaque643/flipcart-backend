73f0da63a941975d63334791abd77647d51b5f75
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-26 19:44:02 +0600

    routes + controller setup for getProductDetailsById

--- src/routes/product.js
+++ src/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { requiredSignin, adminMiddleware } = require('../common-middleware');
 // const { addCategory, getCategories } = require('../controllers/category');
-const { createProduct, getProductsBySlug } = require('../controllers/product');
+const { createProduct, getProductsBySlug, getProductDetailsById } = require('../controllers/product');
 const multer = require('multer');
 const router = express.Router();
 const shortid = require('shortid');
@@ -23,6 +23,8 @@ const upload = multer({ storage });
 router.post('/product/create', requiredSignin, adminMiddleware, upload.array('productPicture'), createProduct);
 
 router.get('/products/:slug', getProductsBySlug);
+
+router.get('/product/:productId', getProductDetailsById);
 
 
 // router.get('/category/getcategory', getCategories);
