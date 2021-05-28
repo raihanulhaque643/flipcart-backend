9bb968237f43db26af54152344f3a006f357bbf9
Raihanul Haque <raihan.hd.500@gmail.com>
2021-05-28 15:19:54 +0600

    cart route,model and controllers updated/added

--- src/routes/cart.js
+++ src/routes/cart.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { requiredSignin, userMiddleware } = require('../common-middleware');
-const { addItemToCart } = require('../controllers/cart');
+const { addItemToCart, getCartItems } = require('../controllers/cart');
 const router = express.Router();
 
 router.post('/user/cart/addtocart', requiredSignin, userMiddleware, addItemToCart);
+
+router.post('/user/getCartItems', requiredSignin, userMiddleware, getCartItems);
 
 module.exports = router;
